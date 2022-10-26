090ed10db654f54577bb4f1a3761e5c4801b6ce1
AlchemistFlekt <knidhi530@gmail.com>
2022-10-26 12:29:23 -0400

    add score tracking for each game

--- script.js
+++ script.js
@@ -6,41 +6,50 @@ function getComputerChoice(){
             console.log("rock");
             return "rock";
     }
-    else if(compChoice==1){                          //branch for Paper
+    else if(compChoice==1){                  //branch for Paper
             console.log("paper");
             return "paper";
     }
-    else{                         //branch for Scissors
+    else{                                   //branch for Scissors
             console.log("scissors");
             return "scissors";
     }
 }
 
-function playRound(playerSelection, computerSelection){              //function to determine outcome of round
+function playRound(playerSelection, computerSelection, score){              //function to determine outcome of round, print outcome to console, and updte score
     if(playerSelection=="rock" && computerSelection=="paper"){
-        console.log("You Lose! Paper beats Rock");
+        console.log("You Lose! Paper beats Rock");                          
+        return score-1;
     }
     else if(playerSelection=="rock"&& computerSelection=="scissors"){
         console.log("You Win! Rock beats Scissors");
+        return score+1;
+        
     }
     else if(playerSelection=="scissors"&& computerSelection=="paper"){
         console.log("You Win! Scissors beats Paper");
+        return score+1;
     }
     else if(playerSelection=="scissors"&& computerSelection=="rock"){
         console.log("You Lose! Rock beats Scissors");
+        return score-1;
     }
     else if(playerSelection=="paper"&& computerSelection=="scissors"){
         console.log("You Lose! Scissors beats Paper");
+        return score-1;
     }
     else if(playerSelection=="paper"&& computerSelection=="rock"){
         console.log("You Win! Paper beats Rock");
+        return score+1;
     }
     else{                           //If player and computer choose same option
         console.log("It's a draw!");
+        return score;
     }
 
 }
-function game(){                                                        //play a 5 round game
+function game(){
+    let score=0;                                                        //play a 5 round game
     for(let i= 0; i<5; i++){                                            //loop for playing multiple rounds, stopping after fifth round
         let userInput= prompt("Choose Rock, Paper, or Scissors");       //prompt player to choose option
         playerSelection= userInput.toLowerCase();                       //convert user input to lowercase to avoid case sensitivity
@@ -57,7 +66,17 @@ function game(){                                                        //play a
 
         console.log(playerSelection);
         let computerSelection= getComputerChoice();
-        playRound(playerSelection, computerSelection);
+        score= playRound(playerSelection, computerSelection, score);
+    }
+    //compare if player has more round wins than computer
+    if(score > 0){        //branch for positive score, meaning player had more wins                
+        console.log("Congratualtions! You have won the game."); //Print winning message to console
+    }
+    else if(score < 0){  //branch for negtive score meaning computer has more wins
+        console.log("You have lost the game.");                 //Print losing message to console
+    }
+    else{              //branch if score equals 0 meaning player and computer have tied
+        console.log("We have tied!");                           //print tied message to console
     }
 }
 
